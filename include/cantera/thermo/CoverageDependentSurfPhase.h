3f8ce307f9ca2de2ffebd7d90b6762757ab763ce
Jongyoon Bae <jongyoon_bae@brown.edu>
2021-03-03 17:31:09 -0500

    check point 3.3.21

--- include/cantera/thermo/CoverageDependentSurfPhase.h
+++ include/cantera/thermo/CoverageDependentSurfPhase.h
@@ -41,9 +41,88 @@ public:
     virtual void getPartialMolarEnthalpies(doublereal* hbar) const;
     virtual void getPartialMolarEntropies(doublereal* sbar) const;
     virtual void getPartialMolarCp(doublereal* cpbar) const;
-    //PartialMolarVolumes SAME as SurfPhase: MolarVolume = MolarArea
-    virtual void getPartialMolarVolumes(doublereal* vbar) const;
+    
+    //SAME
+    //virtual void getPartialMolarVolumes(doublereal* vbar) const;
+    
     virtual void getStandardChemPotentials(doublereal* mu0) const;
+
+    //SAME
+    //virtual void getActivityConcentrations(doublereal* c) const;
+    //virtual doublereal standardConcentration(size_t k = 0) const;
+    //virtual doublereal logStandardConc(size_t k=0) const;
+    //virtual void setParameters(int n, doublereal* const c);
+    //virtual void setParametersFromXML(const XML_Node& thermoData);
+
+    //SAME
+    //virtual void initThermo();
+    //virtual bool addSpecies(shared_ptr<Species> spec);
+    //virtual void setStateFromXML(const XML_Node& state);
+    //virtual void setState(const AnyMap& state);
+    //doublereal siteDensity()
+    //virtual double size(size_t k) const
+    //void setSiteDensity(doublereal n0);
+
+    virtual void getGibbs_RT(doublereal* grt) const;
+    virtual void getEnthalpy_RT(doublereal* hrt) const;
+    virtual void getEntropy_R(doublereal* sr) const;
+    virtual void getCp_R(doublereal* cpr) const;
+
+    //SAME
+    //virtual void getStandardVolumes(doublereal* vol) const;
+    //virtual doublereal pressure() const
+    //virtual void setPressure(doublereal p)
+
+    //SAME thermodynamic properties at the standard state
+    //virtual void getPureGibbs(doublereal* g) const;
+    //virtual void getGibbs_RT_ref(doublereal* grt) const;
+    //virtual void getEnthalpy_RT_ref(doublereal* hrt) const;
+    //virtual void getEntropy_R_ref(doublereal* er) const;
+    //virtual void getCp_R_ref(doublereal* cprt) const;
+
+    //SAME
+    //void setCoverages(const doublereal* theta);
+    //void setCoveragesNoNorm(const doublereal* theta);
+    //void setCoveragesByName(const std::string& cov);
+    //void setCoveragesByName(const compositionMap& cov);
+    
+    //SAME
+    //void getCoverages(doublereal* theta) const;
+    
+protected:
+    //! Surface site density (kmol m-2)
+    doublereal m_n0;
+
+    //! Vector of species sizes (number of sites occupied). length m_kk.
+    vector_fp m_speciesSize;
+
+    //! log of the surface site density
+    doublereal m_logn0;
+
+    //! Current value of the pressure (Pa)
+    doublereal m_press;
+
+    //! Temporary storage for the reference state enthalpies
+    mutable vector_fp m_h0;
+
+    //! Temporary storage for the reference state entropies
+    mutable vector_fp m_s0;
+
+    //! Temporary storage for the reference state heat capacities
+    mutable vector_fp m_cp0;
+
+    //! Temporary storage for the reference state Gibbs energies
+    mutable vector_fp m_mu0;
+
+    //! Temporary work array
+    mutable vector_fp m_work;
+
+    //! vector storing the log of the size of each species.
+    /*!
+     * The size of each species is defined as the number of surface sites each
+     * species occupies.
+     */
+    mutable vector_fp m_logsize;
 
 private:
     //! Update the species reference state thermodynamic functions
--- src/thermo/CoverageDependentSurfPhase.cpp
+++ src/thermo/CoverageDependentSurfPhase.cpp
@@ -15,30 +15,103 @@ using namespace std;
 
 namespace Cantera
 {
-
-doublereal CoverageDependentSurfPhase::entropy_mole() const
+CoverageDependentSurfPhase::CoverageDependentSurfPhase(doublereal n0):
+    m_press(OneAtm)
 {
-    return 1234.5678;
+    setSiteDensity(n0);
+    setNDim(2);
+}
+
+CoverageDependentSurfPhase::CoverageDependentSurfPhase(const std::string& infile, const std::string& id_):
+    m_press(OneAtm)
+{
+    initThermoFile(infile, id_);
+}
+
+CoverageDependentSurfPhase::CoverageDependentSurfPhase(XML_Node& xmlphase):
+    m_press(OneAtm)
+{
+    importPhase(xmlphase, this);
+}
+
+doublereal CoverageDependentSurfPhase::enthalpy_mole() const
+{
+    if (m_n0 <= 0.0) {
+        return 0.0;
+    }
+    _updateThermo();
+    return mean_X(m_h0);
 }
 
 /*!
-*void CoverageDependentSurfPhase::_updateThermo(bool force) const
-*{
-*    doublereal tnow = temperature();
-*    doublereal covnow = coverage();
-*    if (m_tlast != tnow || m_covlast != covnow || force) {
-*        m_spthermo.update(tnow, m_cp0.data(), m_h0.data(), m_s0.data());
-*        m_tlast = tnow;
-*        m_covlast = covnow;
-*        for (size_t k = 0; k < m_kk; k++) {
-*            m_h0[k] *= GasConstant * tnow;
-*            m_s0[k] *= GasConstant;
-*            m_cp0[k] *= GasConstant;
-*            m_mu0[k] = m_h0[k] - tnow*m_s0[k];
-*        }
-*        m_tlast = tnow;
-*    }
-*}
+doublereal CoverageDependentSurfPhase::entropy_mole() const
+{
+
+}
+
+doublereal CoverageDependentSurfPhase::cp_mole() const
+{
+
+}
+
+void CoverageDependentSurfPhase::getChemPotentials(doublereal* mu) const
+{
+
+}
+void CoverageDependentSurfPhase::getPartialMolarEnthalpies(doublereal* hbar) const
+{
+
+}
+void CoverageDependentSurfPhase::getPartialMolarEntropies(doublereal* sbar) const
+{
+
+}
+void CoverageDependentSurfPhase::getPartialMolarCp(doublereal* cpbar) const
+{
+
+}
+
+void CoverageDependentSurfPhase::getStandardChemPotentials(doublereal* mu0) const
+{
+
+}
+
+void CoverageDependentSurfPhase::getGibbs_RT(doublereal* grt) const
+{
+
+}
+void CoverageDependentSurfPhase::getEnthalpy_RT(doublereal* hrt) const
+{
+
+}
+void CoverageDependentSurfPhase::getEntropy_R(doublereal* sr) const
+{
+
+}
+void CoverageDependentSurfPhase::getCp_R(doublereal* cpr) const
+{
+
+}
 */
+
+
+void CoverageDependentSurfPhase::_updateThermo(bool force) const
+{
+    doublereal tnow = temperature();
+    doublereal covnow = coverage();
+    if (m_tlast != tnow || m_covlast != covnow || force) {
+        m_spthermo.update(tnow, m_cp0.data(), m_h0.data(), m_s0.data());
+        m_tlast = tnow;
+        m_covlast = covnow;
+        for (size_t k = 0; k < m_kk; k++) {
+            m_h0[k] *= GasConstant * tnow;
+            m_s0[k] *= GasConstant;
+            m_cp0[k] *= GasConstant;
+            m_mu0[k] = m_h0[k] - tnow*m_s0[k];
+        }
+        m_tlast = tnow;
+    }
+}
+
 
 }
